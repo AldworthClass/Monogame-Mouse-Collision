faa478c83c2e8cc5f99a867263200893fc33f280
Steve Aldworth <s.aldworth@tvdsb.ca>
2021-10-01 14:27:42 -0400

    Mostly done

--- Game1.cs
+++ Game1.cs
@@ -9,19 +9,30 @@ namespace Monogame_Mouse_Collision
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        MouseState mouseState;
+        MouseState previousMouseState;
+        Vector2 mouseLocation;
+
         Texture2D crosshairTexture;
         Rectangle crosshairRect;
+
+        Texture2D targetTexture;
+        Rectangle targetRect;
+
+        SpriteFont statusFont;
+        string status;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+
 
             base.Initialize();
         }
@@ -31,14 +42,36 @@ namespace Monogame_Mouse_Collision
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            crosshairTexture = Content.Load<Texture2D>("crosshair");
+            crosshairRect = new Rectangle(0, 0, 40, 40);
+
+            targetTexture = Content.Load<Texture2D>("targetSquare");
+            targetRect = new Rectangle(300, 140, 200, 200);
+
+            statusFont = Content.Load<SpriteFont>("StatusFont");
+            status = "Nothing";
         }
 
         protected override void Update(GameTime gameTime)
         {
+            mouseState = Mouse.GetState();
+            crosshairRect.Location = mouseState.Position;
+            crosshairRect.Offset(-20, -20);
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+
+            if (targetRect.Contains(mouseState.Position) && mouseState.LeftButton == ButtonState.Pressed)
+            {
+                status = "Hit";
+            }
+            else if(mouseState.LeftButton == ButtonState.Pressed)
+            {
+                status = "Missed";
+            }
+
 
             base.Update(gameTime);
         }
@@ -48,6 +81,11 @@ namespace Monogame_Mouse_Collision
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(statusFont, status, new Vector2(10, _graphics.PreferredBackBufferHeight - 40), Color.Black);
+            _spriteBatch.Draw(targetTexture, targetRect, Color.White);
+            _spriteBatch.Draw(crosshairTexture, crosshairRect, Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
